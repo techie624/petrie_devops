c774a5a075fcf43f1526c44c426e8c27ba843157
rlpetrie <roy.petrie@nedap.com>
2023-09-15 15:45:28 -0400

    env?

--- main.tf
+++ main.tf
@@ -13,22 +13,51 @@ resource "aws_instance" "my_instance" {
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
   user_data = <<-EOF
-              #!/bin/bash
-              # For the default ubuntu user
-              echo "${var.SSH_PUBLIC_KEY}" > /home/ubuntu/.ssh/authorized_keys
-              chmod 600 /home/ubuntu/.ssh/authorized_keys
-              chown ubuntu:ubuntu /home/ubuntu/.ssh/authorized_keys
+                #!/bin/bash
+                # For the default ubuntu user
+                echo "${var.SSH_PUBLIC_KEY}" > /home/ubuntu/.ssh/authorized_keys
+                chmod 600 /home/ubuntu/.ssh/authorized_keys
+                chown ubuntu:ubuntu /home/ubuntu/.ssh/authorized_keys
 
-              # Create the rpetrie user
-              useradd rpetrie -m -s /bin/bash
-              echo "rpetrie ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+                # Create the rpetrie user
+                useradd rpetrie -m -s /bin/bash
+                echo "rpetrie ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 
-              # Set up SSH for rpetrie
-              mkdir /home/rpetrie/.ssh
-              echo "${var.SSH_PUBLIC_KEY}" > /home/rpetrie/.ssh/authorized_keys
-              chmod 700 /home/rpetrie/.ssh
-              chmod 600 /home/rpetrie/.ssh/authorized_keys
-              chown -R rpetrie:rpetrie /home/rpetrie/.ssh
+                # Set up SSH for rpetrie
+                mkdir /home/rpetrie/.ssh
+                echo "${var.SSH_PUBLIC_KEY}" > /home/rpetrie/.ssh/authorized_keys
+                chmod 700 /home/rpetrie/.ssh
+                chmod 600 /home/rpetrie/.ssh/authorized_keys
+                chown -R rpetrie:rpetrie /home/rpetrie/.ssh
+
+                # Update the system
+                apt-get update
+
+                # Install the requested packages
+                apt-get install -y vim tree htop tmux curl git
+
+                # Install Docker
+                apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+                curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+                add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+                apt-get update
+                apt-get install -y docker-ce
+                usermod -aG docker rpetrie
+
+                # Set hostname
+                hostnamectl set-hostname testing
+
+                # Append custom lines to rpetrie's .bashrc
+                cat <<EOL >> /home/rpetrie/.bashrc
+
+                # Custom settings
+                alias ll="ls -larth"
+                alias ..="cd .."
+                EOL
+
+                # Ensure ownership is correct
+                chown rpetrie:rpetrie /home/rpetrie/.bashrc
+
               EOF
 
   tags = {
