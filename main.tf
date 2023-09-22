ad8e7deb43ef00969a275532e0ff123f030329f9
rlpetrie <roy.petrie@nedap.com>
2023-09-22 11:22:40 -0400

    testing with security groups

--- main.tf
+++ main.tf
@@ -4,13 +4,39 @@ provider "aws" {
   secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
+resource "aws_security_group" "example" {
+  name        = "example"
+  description = "Allow SSH inbound and all outbound traffic"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "example"
+  }
+}
+
 resource "aws_instance" "my_instance" {
   ami           = "ami-053b0d53c279acc90" # Ubuntu 22.04 AMI ID (64-bit)
   instance_type = "t2.micro"
 
+  security_groups = [aws_security_group.example.name]
+
   key_name = aws_key_pair.my_key.key_name
 
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  vpc_security_group_ids = [aws_security_group.example.id]
+
 
   user_data = <<-EOF
                 #!/bin/bash
@@ -62,18 +88,6 @@ resource "aws_instance" "my_instance" {
 
   tags = {
     Name = "TestingInstance"
-  }
-}
-
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
