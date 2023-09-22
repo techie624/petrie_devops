c399ff4c213de2decdc2f073862a258fb6be044d
rlpetrie <roy.petrie@nedap.com>
2023-09-22 13:59:11 -0400

    ethorian_net_home

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "instance_public_ip" {
   description = "The public IP address of the EC2 instance"
-  value       = aws_instance.my_instance.public_ip
+  value       = aws_instance.ethorian_net_home.public_ip
 }
