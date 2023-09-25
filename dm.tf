d17f0364eb8986469bc2524aedf4cd8efec41848
rlpetrie <roy.petrie@nedap.com>
2023-09-25 17:19:06 -0400

    dm

--- dm.tf
+++ dm.tf
@@ -145,7 +145,7 @@ resource "aws_instance" "ethorian_net_dm" {
   }
 }
 
-resource "aws_route53_record" "subdomain_record" {
+resource "aws_route53_record" "subdomain_record_dm" {
   zone_id = var.ETHORIAN_NET_HOSTED_ZONE_ID
   name    = "dm.ethorian.net"
   type    = "A"
