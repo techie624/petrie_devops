03b88d13c27dcd7b1379eb4ac4acb15c81e76755
rlpetrie <roy.petrie@nedap.com>
2023-10-09 12:08:34 -0400

    update userdata, each line is its own script command

--- home.tf
+++ home.tf
@@ -148,49 +148,43 @@ resource "aws_instance" "ethorian_net_home" {
                 chown rpetrie:rpetrie /home/rpetrie/workspace
 
                 # disable host key checking
-                echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> /home/rpetrie/.ssh/config
+                su rpetrie && \
+                echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> /home/rpetrie/.ssh/config && \
                 ssh-keyscan github.com >> /home/rpetrie/.ssh/known_hosts
 
                 sleep 5
 
                 # Clone the repository
-                su rpetrie
-                
-                echo;
-                echo "Current User: $(whoami)"
-                echo "Current dir: $(pwd)"
-
-                echo;
-
-                cd /home/rpetrie/workspace
-                git clone git@github.com:techie624/ethoria_saga.git
-
-                # Run the script to start the container
+                su rpetrie && \
+                echo; && \
+                echo "Current User: $(whoami)" && \
+                echo "Current dir: $(pwd)" && \
+                echo;  && \
+                cd /home/rpetrie/workspace  && \
+                git clone git@github.com:techie624/ethoria_saga.git  && \
                 bash /home/rpetrie/workspace/ethoria_saga/run.sh
 
                 # Set up the cron job
+                su rpetrie && \
                 echo "0 * * * * /bin/bash /home/rpetrie/workspace/ethoria_saga/git_pull.sh >> /home/rpetrie/pull.log 2>&1" | crontab -
 
                 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
                 ### Clone repo and run container for site dm
 
                 # Clone the repository
-                cd /home/rpetrie/workspace
-
-                echo;
-                echo "Current User: $(whoami)"
-                echo "Current dir: $(pwd)"
-
-                echo;
-
-                git clone git@github.com:techie624/ethoria_dm.git
-
-                # Run the script to start the container
-                cd /home/rpetrie/workspace/ethoria_dm
-                htpasswd -cb .htpasswd ${var.HTPASSWD_USER} ${var.HTPASSWD_PASS}
+                su rpetrie && \
+                cd /home/rpetrie/workspace && \
+                echo; && \
+                echo "Current User: $(whoami)" && \
+                echo "Current dir: $(pwd)" && \
+                echo; && \
+                git clone git@github.com:techie624/ethoria_dm.git && \
+                cd /home/rpetrie/workspace/ethoria_dm && \
+                htpasswd -cb .htpasswd ${var.HTPASSWD_USER} ${var.HTPASSWD_PASS} && \
                 bash run.sh
 
                 # Set up the cron job
+                su rpetrie && \
                 echo "0 * * * * /bin/bash /home/rpetrie/workspace/ethoria_dm/git_pull_deploy.sh >> /home/rpetrie/pull.log 2>&1" | crontab -
 
                 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
