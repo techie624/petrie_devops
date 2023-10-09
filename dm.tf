3cc5cd70b657976daf7dac85eb71ed4b9a4d0609
rlpetrie <roy.petrie@nedap.com>
2023-10-09 10:56:23 -0400

    asdf

--- dm.tf
+++ dm.tf
@@ -12,6 +12,8 @@ resource "aws_instance" "ethorian_net_dm" {
   user_data = <<-EOF
                 #!/bin/bash
 
+                # comment change to trigger deploy 001
+
                 # user_data takes a couple minutes to finish
                 # tail -f /var/log/cloud-init-output.log
 
